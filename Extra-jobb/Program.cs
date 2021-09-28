e50fac21fba8a615c6d1445cfd05cc9f7737e0c9
Teo Wiederkehr <teo.wiederkehr@hotmail.com>
2021-09-28 14:48:25 +0200

    fryifgtuftyug+yutolfrtjfyfgufrtjdr67y

---
+++ Extra-jobb/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+String name;
+string answer;
+string Man;
+string Woman;
+string Helicopeter;
+string Dog;
+
+Console.WriteLine("Hello mister Chose your name");
+
+name = Console.ReadLine();
+
+Console.WriteLine("Now chose your gender");
+Console.WriteLine("1.Man");
+Console.WriteLine("2.Woman");
+Console.WriteLine("3.Helicopeter");
+Console.WriteLine("4.Dog");
+Console.WriteLine("or any other of the other million genders?");
+
+Man = Console.ReadLine();
+Woman = Console.ReadLine();
+Helicopeter = Console.ReadLine();
+Dog = Console.ReadLine();
+
+
+Console.WriteLine("!Amogus meeting!");
+
+Console.ReadLine();
+
+Console.WriteLine($"Black: I found purple dead in communications and i saw {name} vent!");
+
+Console.ReadLine();
+
+
+
+Console.WriteLine("White: It Sus");
+Console.ReadLine();
+Console.WriteLine($"Pink: This {Helicopeter}{Man}{Dog}{Woman} Sus");
+Console.ReadLine();
+
+Console.WriteLine($"Green: Yes your right this  {Helicopeter}{Man}{Dog}{Woman} muchos susos");
+Console.ReadLine();
+Console.WriteLine("Red: Hes name is Sus");
+Console.ReadLine();
+Console.WriteLine($"Blue: I can vouch for {name} The {Helicopeter}{Man}{Dog}{Woman} He was in Electric with me");
+Console.ReadLine();
+
+Console.WriteLine($"Red: Not enough proof you both can be impostors. {name} whats your defence?");
+
+answer = Console.ReadLine();
+
+Console.WriteLine("You have been ejected.....");
+Console.ReadLine();
+
+Console.WriteLine("Green: He was muchos susos indeed");
+Console.ReadLine();
+
+Console.WriteLine("The End");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
